e8b60c68f3fe9d1eb5e8dfecccd6ce50b3b655aa
Vigneshwaran <vigneshwaranmca22@gmail.com>
2025-05-26 14:29:03 +0530

    update the backend deploy link into frontend axios

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -11,7 +11,7 @@ export default function App() {
   useEffect(() => {
     const fetchReminders = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/reminders')
+        const res = await axios.get('https://reminder-ve5p.onrender.com//api/reminders')
         setReminders(res.data.data) // backend returns { success, data }
       } catch (err) {
         console.error("Error fetching reminders:", err.message)
@@ -26,7 +26,7 @@ export default function App() {
     console.log('Sending reminder:', { message: text, reminder_datetime: datetime, reminder_type: method })
 
     try {
-      const res = await axios.post('http://localhost:5000/api/reminders', {
+      const res = await axios.post('https://reminder-ve5p.onrender.com//api/reminders', {
         message: text,
         reminder_datetime: datetime,
         reminder_type: method,
