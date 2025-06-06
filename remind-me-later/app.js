88fcafb36852ec31faaa1e4444f04c552b412589
Vigneshwaran <vigneshwaranmca22@gmail.com>
2025-06-06 14:08:04 +0530

    edit the local host url into deployed frontedn url

--- remind-me-later/app.js
+++ remind-me-later/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // --- CORS setup ---
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://reminder-2-2ktk.onrender.com/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
   credentials: true
